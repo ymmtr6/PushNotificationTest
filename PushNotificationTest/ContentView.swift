a1792930ed813b832de00b305ca78774149a987f
Riku <ymmtr666@gmail.com>
2024-04-14 21:35:57 +0900

    Initial Commit

---
+++ PushNotificationTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PushNotificationTest
+//
+//  Created by Riku Yamamoto on 2024/04/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PushNotificationTest/PushNotificationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationTestApp.swift
+//  PushNotificationTest
+//
+//  Created by Riku Yamamoto on 2024/04/14.
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
